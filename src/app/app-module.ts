4bd39e0250d2081e7478912ffe7006bf2e4e3742
marcospedroche05 <marcospedroche62@gmail.com>
2025-11-05 13:51:01 +0100

    rutas angular

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,17 +1,30 @@
+import { routing, appRoutingProvider } from './app.routing';
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
+import { HomeComponent } from './components/home.component/home.component';
+import { MusicaComponent } from './components/musica.component/musica.component';
+import { CineComponent } from './components/cine.component/cine.component';
+import { MenuComponent } from './components/menu.component/menu.component';
+import { NotFoundComponent } from './components/not-found.component/not-found.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HomeComponent,
+    MusicaComponent,
+    CineComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    appRoutingProvider
   ],
   bootstrap: [App]
 })
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { HomeComponent } from "./components/home.component/home.component";
+import { CineComponent } from "./components/cine.component/cine.component";
+import { MusicaComponent } from "./components/musica.component/musica.component";
+//NECESITAMOS UNA SERIE DE MODULOS QUE SE ENCUENTRAN DENTRO DE ANGULAR/ROUTER
+import { Routes, RouterModule } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
+import { NotFoundComponent } from "./components/not-found.component/not-found.component";
+
+//NECESITAMOS UN ARRAY CON LAS RUTAS, DICHO ARRAY SERA DE TIPO Routes
+const appRoutes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "cine", component: CineComponent },
+    { path: "musica", component: MusicaComponent },
+    { path: "**", component: NotFoundComponent}
+]
+
+//DESDE ESTA CLASE DEBEMOS EXPORTAR EL ARRAY DE ROUTES COMO PROVIDER
+export const appRoutingProvider: any[] = [];
+//LAS PROPIAS RUTAS A EXPORTAR
+export const routing: ModuleWithProviders<any> = 
+RouterModule.forRoot(appRoutes);
